27097adedd83c793d3beb0615dea14fce62165f5
Mateusz Kaszubowski <m.aizenvolt@gmail.com>
2024-12-31 12:26:34 +0100

    [Task 7] Repository abstraction

---
+++ Assets/Src/Repositories/Repository.cs
@@ -0,0 +1,35 @@
+﻿// /*
+//  * Copyright © 2024 Virtual Recovery
+//  * Author: Mateusz Kaszubowski
+//  * Created on: 31/12/2024
+//  */
+
+using System.Collections.Generic;
+
+namespace VirtualRecovery {
+    internal abstract class Repository<T> {
+        protected DbConnector m_dbConnector;
+
+        protected Repository() {
+            m_dbConnector = new DbConnector();
+            m_dbConnector.OpenConnection();
+            EnsureTable();
+        }
+
+        protected abstract string GetTableName();
+
+        private void EnsureTable() {
+            if (!m_dbConnector.TableExists(GetTableName())) {
+                m_dbConnector.ExecuteNonQuery(GenerateCreateTableQuery());
+            }
+        }
+
+        protected abstract string GenerateCreateTableQuery();
+
+        public abstract void Insert(T entity);
+        public abstract void Update(int id, T entity);
+        public abstract void Delete(int id);
+        public abstract T GetById(int id);
+        public abstract List<T> GetAll();
+    }
+}
